da18bc3a6cd490a5e4902e17f2f83260641894a2
Rupert Angermeier <rupert.angermeier@gmail.com>
2014-02-10 14:43:02 +0100

    implement formatting for LESS files
    - rename functions and classes
    - remove different formatting modes and keymaps for them
    - change indentation to spaces
    - adapt regular expression for LESS code:
    -- curly brackets can appear inside brackets
    -- iterate over lines to set identation
    - update README

--- README.md
+++ README.md
@@ -1,74 +1,12 @@
-CSS Formatting for Sublime Text
+LESS Formatting for Sublime Text
 ===================
 
 
 Description
 -----------
 
-CSS Format is a CSS formatting plugin for Sublime Text, you can convert CSS code to Compact、Expand format, or compress CSS. CSS Format is just only a formatter, do not supports grammar check and auto correct feature.
-
-**Example:**
-
-* Compact format:
-
-        body { background-color: #fff; color: #333; font-size: 12px; }
-        a { color: #06f; }
-        a:hover { color: #09c; }
-
-* Expand format:
-
-        body {
-            background-color: #fff;
-            color: #333;
-            font-size: 12px;
-        }
-        a {
-            color: #06f;
-        }
-        a:hover {
-            color: #09c;
-        }
-
-* Compress:
-
-        body{background-color:#fff;color:#333;font-size:12px;}a{color:#06f;}a:hover{color:#09c;}
-
-
-Installation
-------------
-
-**OPTION 1 - with Package Control (recommended)**
-
-The easiest way to install this package is through Package Control.
-
-1. Install [Package Control](https://sublime.wbond.net/installation), follow instructions on the website.
-
-2. Open command panel: `Ctrl+Shift+P` (Linux/Windows) or `Cmd+Shift+P` (OS X) and select **Package Control: Install Package**.
-
-3. When packages list appears, type `CSS Format` and select it.
-
-
-**OPTION 2 - with Git**
-
-Clone the repository in your Sublime Text "Packages" directory:
-
-    git clone git://github.com/mutian/CSS-Format.git "CSS Format"
-
-You can find your "Packages" inside the following directories:
-
-* OS X:
-    `~/Library/Application Support/Sublime Text 2/Packages/`
-
-* Windows:
-    `%APPDATA%/Sublime Text 2/Packages/`
-
-* Linux:
-    `~/.Sublime Text 2/Packages/`
-
-
-**OPTION 3 - without Git**
-
-Download the latest source zip from [Github](https://github.com/mutian/CSS-Format) and extract it into a new folder named `CSS Format` in your Sublime Text "Packages" folder.
+LESS Format is a LESS formatting plugin for Sublime Text. It does a little 
+normalization and indents the code according to the current document settings.
 
 
 Usage
@@ -76,32 +14,21 @@ Usage
 
 Select the code, or place cursor in the document, and execute commands in one of the following ways:
 
-* Use "Edit" menu: **Edit &gt; CSS Format**.
+* Use "Edit" menu: **Edit &gt; Format LESS**.
 
-* Keyboard shortcuts:
+* Keyboard shortcuts `Ctrl+Alt+[`
+    Mac user can also use the keyboard shortcut `Ctrl+Cmd+[`
 
-    * `Ctrl+Alt+[` : Convert to compact format
-    * `Ctrl+Alt+]` : Convert to expand format
-    * `Ctrl+Alt+\` : Compress CSS
-
-    Mac user can also use the following keyboard shortcuts:
-
-    * `Ctrl+Cmd+[` : Convert to compact format
-    * `Ctrl+Cmd+]` : Convert to expand format
-    * `Ctrl+Cmd+\` : Compress CSS
-
-* Use command panel: Open command panel: `Ctrl+Shift+P` (Linux/Windows) or `Cmd+Shift+P` (OS X) and select **CSS Format: XXX**.
+* Use command panel: Open command panel: `Ctrl+Shift+P` (Linux/Windows) or `Cmd+Shift+P` (OS X) and select **Format LESS**.
 
 
 Author
 ------
 
-Created by **Mutian** ([http://mutian.info](http://mutian.info/)).
-
-For more info, you can send email to me: mutian(a)me.com!
+Created by **Rupert Angermeier** ([ngrmr.at/](http://ngrmr.at/) / [github.com/rangermeier](https://github.com/rangermeier)).
 
 
 Acknowledgements
 ----------------
 
-Thanks to the **RIA Team** of [Weibo.com](http://weibo.com/) .
+Based on `CSS-Format` by **Mutian** ([github.com/mutian/CSS-Format](https://github.com/mutian/CSS-Format)).
--- less_format.py
+++ less_format.py
@@ -1,96 +1,88 @@
 import sublime, sublime_plugin, re
 
-class CssFormatCommand(sublime_plugin.TextCommand):
-	def run(self, edit, action='compact'):
-		view = self.view
+class LessFormatCommand(sublime_plugin.TextCommand):
+    def run(self, edit, action='compact'):
+        view = self.view
 
-		if view.is_loading():
-			sublime.status_message("Waiting for loading.")
-			return False
+        if view.is_loading():
+            sublime.status_message("Waiting for loading.")
+            return False
 
-		selection = view.sel()[0]
-		if len(selection) > 0:
-			self.format_selection(edit, action)
-		else:
-			self.format_whole_file(edit, action)
+        selection = view.sel()[0]
+        if len(selection) > 0:
+            self.format_selection(edit, action)
+        else:
+            self.format_whole_file(edit, action)
 
-	def format_selection(self, edit, action):
-		view = self.view
-		regions = []
-		for sel in view.sel():
-			region = sublime.Region(
-				view.line(min(sel.a, sel.b)).a,  # line start of first line
-				view.line(max(sel.a, sel.b)).b   # line end of last line
-			)
-			code = view.substr(region)
-			code = self.process_rules(code, action)
-			#view.sel().clear()
-			view.replace(edit, region, code)
+    def format_selection(self, edit, action):
+        view = self.view
+        regions = []
+        for sel in view.sel():
+            region = sublime.Region(
+                view.line(min(sel.a, sel.b)).a,  # line start of first line
+                view.line(max(sel.a, sel.b)).b   # line end of last line
+            )
+            code = view.substr(region)
+            code = self.process_rules(code, action)
+            view.replace(edit, region, code)
 
-	def format_whole_file(self, edit, action):
-		view = self.view
-		region = sublime.Region(0, view.size())
-		code = view.substr(region)
-		code = self.process_rules(code, action)
-		view.replace(edit, region, code)
+    def format_whole_file(self, edit, action):
+        view = self.view
+        region = sublime.Region(0, view.size())
+        code = view.substr(region)
+        code = self.process_rules(code, action)
+        view.replace(edit, region, code)
 
-	def process_rules(self, code, action):
-		actions = {
-			'compact'	: self.compact_rules,
-			'expand'	: self.expand_rules,
-			'compress'	: self.compress_rules
-		}
-		code = re.sub(r"\s*([\{\}:;,])\s*", r"\1", code)		# remove \s before and after characters {}:;,
-		code = re.sub(r",[\d\s\.\#\+>:]*\{", "{", code)			# remove invalid selector
-		code = re.sub(r";\s*;", ";", code)						# remove superfluous ;
+    def process_rules(self, code, action):
 
-		if action != 'compress':
-			code = re.sub(r"\/\*\s*([\s\S]+?)\s*\*\/", r"/* \1 */", code)	# add space before and after comment content
-			code = re.sub(r"\}\s*(\/\*[\s\S]+?\*\/)\s*", r"}\n\1\n", code)	# add \n before and after outside comment
-			code = re.sub(r",(\S)", r", \1", code)							# add space after ,
-			code = re.sub(r"([A-Za-z-]):([^;\{]+[;\}])", r"\1: \2", code)	# add space after properties' :
-			code = re.sub(r"(http[s]?:) \/\/", r"\1//", code)				# fix space after http[s]:
-			code = re.sub(r"\s*!important", r" !important", code)			# add space before !important
+        code = re.sub(r"\s*([\{\}:;,])\s*", r"\1", code)        # remove \s before and after characters {}:;,
+        code = re.sub(r";\s*;", ";", code)                      # remove superfluous ;
 
-		code = actions[action](code)
+        code = re.sub(r"\/\*\s*([\s\S]+?)\s*\*\/", r"/* \1 */", code)   # add space before and after comment content
+        code = re.sub(r"\}\s*(\/\*[\s\S]+?\*\/)\s*", r"}\n\1\n", code)  # add \n before and after outside comment
+        code = re.sub(r",(\S)", r", \1", code)                          # add space after ,
+        code = re.sub(r"([A-Za-z-]):([^;\{]+[;\}])", r"\1: \2", code)   # add space after properties' :
+        code = re.sub(r"(http[s]?:) \/\/", r"\1//", code)               # fix space after http[s]:
+        code = re.sub(r"\s*!important", r" !important", code)           # add space before !important
 
-		code = re.sub(r"(@import[^;]+;)\s*", r"\1\n", code)		# add \n and remove \t after @import
-		code = re.sub(r"^\s*(\S+(\s+\S+)*)\s*$", r"\1", code)	# remove superfluous \s
-		return code
+        
+        code = re.sub(r"\{\s*(?=([^\"]*\"[^\"]*\")*[^\"]*$)", r" {\n", code)        # add space before { , and add \n after {
+        code = re.sub(r"((@media|@[\w-]*keyframes)[^\{]+\{)\s*", r"\1\n", code)     # remove \t after @media {
+        code = re.sub(r"(\S);([^\}])", r"\1;\n\2", code)                            # add \n after ;
+        code = re.sub(r"\;\s*(\/\*[^\n]*\*\/)\s*", r"; \1\n\t", code)               # fix comment after ;
+        code = re.sub(r"\}\s*(?=([^\"]*\"[^\"]*\")*[^\"]*$)", r"\n}\n", code)       # add \n before and after }
 
-	def compact_rules(self, code):
-		#code = re.sub(r"([^\s])([,\}])([^\n])", r"\1\2\n\3", code)
-		#code = re.sub(r"([^\s]),([^\s])", r"\1, \2", code)		# todo: add \n after selectors' ,
+        code = re.sub(r"(@import[^;]+;)\s*", r"\1\n", code)     # add \n and remove \t after @import
+        code = re.sub(r"^\s*(\S+(\s+\S+)*)\s*$", r"\1", code)   # remove superfluous \s
 
-		code = re.sub(r"(\S)\{(\S)", r"\1 { \2", code)					# add space and after {
-		code = re.sub(r"((@media|@[\w-]*keyframes)[^\{]+\{)\s*", r"\1\n", code)	# add \n after @media {
-		code = re.sub(r"(\S);([^\}])", r"\1; \2", code)					# add space after ;
-		code = re.sub(r"\;\s*(\/\*[^\n]*\*\/)\s*", r"; \1\n\t", code)	# fix comment after ;
-		code = re.sub(r"(\/\*[^\n]*\*\/)\s+\}", r"\1}", code)			# remove \n\t between comment and }
-		code = re.sub(r"(\S)\}", r"\1 }", code)							# add space before }
-		code = re.sub(r"\}", r"}\n", code)								# add \n after }
-		#code = re.sub(r"\{([^\{\}])*\}", compact_rules_brace_block, code)	# todo:
-		return code
+        code = re.sub(r"^\s*(\S+(\s+\S+)*)$", r"\1", code)  # remove \s at begin of lines
 
-	# def compact_rules_brace_block(matches):
-	# 	code = matches[0]
-	# 	code = re.sub(r"(\S):(\S)", r"\1: \2", code)
-	# 	return code
+        # get indentation settings
+        settings = self.view.settings()
+        use_spaces = settings.get("translate_tabs_to_spaces")
+        tab_size = int(settings.get("tab_size", 8))
+        indent_characters = "\t"
+        if use_spaces:
+            indent_characters = " " * tab_size
 
-	def expand_rules(self, code):
-		#code = re.sub(r"(\w),([^\{]+\{)", r"\1,\n\2", code)	# todo: add \n after selectors' ,
-		#code = re.sub(r"(\{[^\{]+),\s+([^\}]+\})", r"\1,\2", code)
+        level = 0
+        indented = []
+        # iterate over lines
+        for line in code.split('\n'):
+            line = line.strip()
+            # increment indentation if there is a } (not in comment, not in quotes)
+            if re.search(r"[^(\/\/)|(\/\*)]*}\s*(?=([^\"]*\"[^\"]*\")*[^\"]*$)", line):
+                level -= 1
 
-		code = re.sub(r"(\S)\{(\S)", r"\1 {\n\t\2", code)				# add space before { , and add \n\t after {
-		code = re.sub(r"((@media|@[\w-]*keyframes)[^\{]+\{)\s*", r"\1\n", code)	# remove \t after @media {
-		code = re.sub(r"(\S);([^\}])", r"\1;\n\t\2", code)				# add \n\t after ;
-		code = re.sub(r"\;\s*(\/\*[^\n]*\*\/)\s*", r"; \1\n\t", code)	# fix comment after ;
-		code = re.sub(r"([^\}])\s*\}", r"\1\n}", code)					# add \n before }
-		code = re.sub(r"\}", r"}\n", code)								# add \n after }
-		return code
-	
-	def compress_rules(self, code):
-		code = re.sub(r"\/\*[\s\S]+?\*\/", "", code)		# remove non-empty comments, /**/ maybe a hack
-		code = re.sub(r"\s*([\{\}:;,])\s*", r"\1", code)	# remove \s before and after characters {}:;, again
-		code = re.sub(r"\s*(!important)", r"\1", code)		# remove space before !important
-		return code
+            # add spaces at beginning of line
+            line = indent_characters * level + line
+
+            # decrement indentation if there is a } (not in comment, not in quotes)
+            if re.search(r"[^(\/\/)|(\/\*)]*{\s*(?=([^\"]*\"[^\"]*\")*[^\"]*$)", line):
+                level += 1
+
+            indented.append(line)
+
+        code = "\n".join(indented)
+
+        return code
